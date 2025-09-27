41fba99b2f5b612fcef98489b9ae6b42c6f97b33
666haowenchen <105464692+666haowenchen@users.noreply.github.com>
2025-09-27 12:26:10 -0400

    9/27/2025

---
+++ Assets/Geo_Quest/Scripts/geocontroller.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class geocontroller : MonoBehaviour
+{
+    string hello = "Hello ";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+
+        string world = "world";
+        Debug.Log(hello + world);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
