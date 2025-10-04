8f2cb3945fcaee7e60c14b7bd01281123be7c065
666haowenchen <105464692+666haowenchen@users.noreply.github.com>
2025-10-04 12:26:53 -0400

    10/4/2025

--- Assets/Geo_Quest/Scripts/geocontroller.cs
+++ Assets/Geo_Quest/Scripts/geocontroller.cs
@@ -1,10 +1,13 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
-public class geocontroller : MonoBehaviour
+public class geocontroller : MonoBehaviour 
 {
     string hello = "Hello ";
-
+    string variable1 = "Hello";
+    private int var1 = 3;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,11 +15,45 @@ public class geocontroller : MonoBehaviour
 
         string world = "world";
         Debug.Log(hello + world);
+
+
+        string variable2 = "World";
+        Debug.Log(variable1 + variable2);
+        public int speed = 5;
+        
+        
     }
 
     // Update is called once per frame
     void Update()
+ {
     {
+      
+        
+        Debug.Log(var1++);
+        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -1, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+        float xInput = Input.GetAxis("Horizontal");
+        Debug.Log(xInput);
+        rb.velocity = new Vector2 (xInput + speed, rb.velocity.y);
 
     }
 }
