1cffe6a5f2a0d24868fdf6756155803da0fca4cb
666haowenchen <105464692+666haowenchen@users.noreply.github.com>
2025-10-11 12:30:37 -0400

    Adds death

--- Assets/Geo_Quest/Scripts/geocontroller.cs
+++ Assets/Geo_Quest/Scripts/geocontroller.cs
@@ -1,16 +1,24 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 public class geocontroller : MonoBehaviour 
 {
     string hello = "Hello ";
     string variable1 = "Hello";
     private int var1 = 3;
+    public int speed = 5;
+    Rigidbody2D body2D;
+    public string nextLevel = "level 2";
+    public string deathScene = "death scene";
+    public int health = 100;
     // Start is called before the first frame update
     void Start()
     {
+        body2D =  GetComponent<Rigidbody2D>();
         Debug.Log("Hello World");
 
         string world = "world";
@@ -19,17 +27,15 @@ public class geocontroller : MonoBehaviour
 
         string variable2 = "World";
         Debug.Log(variable1 + variable2);
-        public int speed = 5;
         
-        
+   
     }
 
     // Update is called once per frame
-    void Update()
- {
-    {
-      
-        
+    void Update(){
+
+
+        /*
         Debug.Log(var1++);
         
         if (Input.GetKeyDown(KeyCode.W))
@@ -51,9 +57,36 @@ public class geocontroller : MonoBehaviour
         {
             transform.position += new Vector3(1, 0, 0);
         }
-        float xInput = Input.GetAxis("Horizontal");
-        Debug.Log(xInput);
-        rb.velocity = new Vector2 (xInput + speed, rb.velocity.y);
+        */
+
+            float xInput = Input.GetAxis("Horizontal");
+            Debug.Log(xInput);
+            body2D.velocity = new Vector2(xInput * speed, body2D.velocity.y);
+       
 
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       switch (collision.tag)
+        {
+            case "Finish":
+                {
+                    SceneManager.LoadScene(nextLevel);
+                    break;
+                }
+            case "Death":
+                {
+                    health -= 10;
+                    if (health <= 0)
+                    {
+                        string thisLevel = SceneManager.GetActiveScene().name;
+                        SceneManager.LoadScene(deathScene);
+                    }
+                    break;
+                }
+        }
+    }
+
 }
+
